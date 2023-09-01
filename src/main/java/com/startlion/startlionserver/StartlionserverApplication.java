f5222c07fef6c78ff75fe1f57334df8f1b15c229
unanchoi <unanc2@gmail.com>
2023-09-01 11:24:33 +0900

    [SETTING] initial update

---
+++ src/main/java/com/startlion/startlionserver/StartlionserverApplication.java
@@ -0,0 +1,13 @@
+package com.startlion.startlionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartlionserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartlionserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startlion/startlionserver/StartlionserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startlion.startlionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartlionserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
