0d9474ac89e338c36c27d024a6ea75a50b06c94d
eunsu02 <regulus_leo08@naver.com>
2023-09-03 19:10:25 +0900

    [SETTING] Project initial settings

---
+++ src/main/java/com/startlion/startlionserver/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.startlion.startlionserver.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "START LION",
+                description = "start lion api명세",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("START LION API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
