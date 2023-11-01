0730da7b8db7f12b6d30895a9aa12ccc5344fd76
jinyoungpark00 <doghong00@naver.com>
2023-11-01 19:29:58 +0900

    [SL-2 FIX] solve conflict

---
+++ src/main/java/com/startlion/startlionserver/dto/response/application/ApplicationGetResponse.java
@@ -0,0 +1,2 @@
+package com.startlion.startlionserver.dto.response.application;public class ApplicationGetResponse {
+}
---
+++ src/main/java/com/startlion/startlionserver/service/ApplicationService.java
@@ -0,0 +1,2 @@
+package com.startlion.startlionserver.service;public class ApplicationService {
+}
