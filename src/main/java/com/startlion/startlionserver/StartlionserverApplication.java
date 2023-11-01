17523e41ba6f915f05670514f85ff9511b4a1345
eunsu02 <regulus_leo08@naver.com>
2023-11-01 15:19:01 +0900

    [SL-3] fix h2 DB connection error

--- src/main/java/com/startlion/startlionserver/StartlionserverApplication.java
+++ src/main/java/com/startlion/startlionserver/StartlionserverApplication.java
@@ -2,6 +2,9 @@ package com.startlion.startlionserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+//@EntityScan("com.startlion.startlionserver.domain")
 
 @SpringBootApplication
 public class StartlionserverApplication {
