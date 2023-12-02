c2c3ccb74090573674f69989de6b2ec96e056443
jinyoungpark00 <doghong00@naver.com>
2023-12-02 10:59:58 +0900

    swagger 추가 설명 및 만든 api만 출력도되록 변경

--- src/main/java/com/startlion/startlionserver/controller/InterviewController.java
+++ src/main/java/com/startlion/startlionserver/controller/InterviewController.java
@@ -3,6 +3,7 @@ package com.startlion.startlionserver.controller;
 
 import com.startlion.startlionserver.dto.response.interview.InterviewResponse;
 import com.startlion.startlionserver.service.InterviewService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -22,12 +23,14 @@ public class InterviewController {
     private final InterviewService interviewService;
 
 
+    @Operation(summary = "interview 정보 조회")
     @GetMapping
     public ResponseEntity<List<InterviewResponse>> getInterviews() {
         val response = interviewService.getInterviews();
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "interviewId로 interview 정보 조회")
     @GetMapping("{interviewId}")
     public ResponseEntity<InterviewResponse> getInterviewById(@PathVariable Long interviewId) {
         val response = interviewService.getInterviewById(interviewId);
--- src/main/java/com/startlion/startlionserver/repository/PartJpaRepository.java
+++ src/main/java/com/startlion/startlionserver/repository/PartJpaRepository.java
@@ -2,6 +2,8 @@ package com.startlion.startlionserver.repository;
 
 import com.startlion.startlionserver.domain.entity.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface PartJpaRepository extends JpaRepository<Part, Long> {
 }
