8d77e11254089658188d1103f85e4ce1a308f81f
jinyoungpark00 <doghong00@naver.com>
2023-12-03 13:23:11 +0900

    [SL-1 CHORE] swagger 설명 추가

--- src/main/java/com/startlion/startlionserver/dto/request/application/ApplicationPage4PutRequest.java
+++ src/main/java/com/startlion/startlionserver/dto/request/application/ApplicationPage4PutRequest.java
@@ -1,8 +1,10 @@
 package com.startlion.startlionserver.dto.request.application;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class ApplicationPage4PutRequest {
+    @Schema(description = "면접 시간", example = "true", required = true)
     private String interview;
 }
