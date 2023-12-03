dc30443744cf7ce55480f95ab52351acdaa35e07
jinyoungpark00 <doghong00@naver.com>
2023-12-03 13:23:11 +0900

    [SL-1 CHORE] swagger 설명 추가

--- src/main/java/com/startlion/startlionserver/dto/request/application/ApplicationPage3PutRequest.java
+++ src/main/java/com/startlion/startlionserver/dto/request/application/ApplicationPage3PutRequest.java
@@ -1,13 +1,21 @@
 package com.startlion.startlionserver.dto.request.application;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ApplicationPage3PutRequest {
+    @Schema(description = "파트 질문 답변1", example = "true", required = true)
     private String partAnswer1;
+
+    @Schema(description = "파트 질문 답변2", example = "true", required = true)
     private String partAnswer2;
+
+    @Schema(description = "파트 질문 답변3", example = "true", required = true)
     private String partAnswer3;
+
+    @Schema(description = "포트폴리오", example = "true", required = true)
     private String portfolio;
 }
