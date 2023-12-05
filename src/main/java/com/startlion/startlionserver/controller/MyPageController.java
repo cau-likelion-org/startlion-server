3d71a0911dfafb4d4e0a120de95f19e05e39c5b3
jinyoungpark00 <doghong00@naver.com>
2023-12-05 22:22:51 +0900

    [SL-4 CHORE] my-page url 변경

--- src/main/java/com/startlion/startlionserver/controller/MyPageController.java
+++ src/main/java/com/startlion/startlionserver/controller/MyPageController.java
@@ -1,6 +1,5 @@
 package com.startlion.startlionserver.controller;
 
-import com.startlion.startlionserver.dto.response.application.ApplicationListGetResponse;
 import com.startlion.startlionserver.dto.response.application.ApplicationListWithSubmittedResponse;
 import com.startlion.startlionserver.service.MyPageService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,15 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/myPage")
+@RequestMapping(value = "/my-page")
 @RequiredArgsConstructor
 public class MyPageController {
 
     private final MyPageService mypageService;
 
     @Operation(summary = "myPage 정보 조회")
-    @GetMapping("{userId}")
+    @GetMapping("/users/{userId}")
     public ResponseEntity<ApplicationListWithSubmittedResponse> getApplicationList(@PathVariable Long userId) {
+
         ApplicationListWithSubmittedResponse response = mypageService.getApplicationList(userId);
         return ResponseEntity.ok(response);
     }
