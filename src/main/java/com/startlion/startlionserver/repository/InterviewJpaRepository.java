1605af2c9e4c4fc83160542ba0d906d98a971e2b
unanchoi <unanc2@gmail.com>
2023-12-06 20:56:16 +0900

    [SL-10 CHORE] Swagger 관련 의존성 교체 및 설정 클래스 수정

--- src/main/java/com/startlion/startlionserver/repository/InterviewJpaRepository.java
+++ src/main/java/com/startlion/startlionserver/repository/InterviewJpaRepository.java
@@ -2,7 +2,6 @@ package com.startlion.startlionserver.repository;
 
 import com.startlion.startlionserver.domain.entity.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface InterviewJpaRepository extends JpaRepository<Interview, Long> {
 }
