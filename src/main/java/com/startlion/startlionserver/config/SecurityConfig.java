dd1d01f318cd4969b34fbb5cdb4f2877b4d51c42
jinyoungpark00 <doghong00@naver.com>
2023-12-06 20:46:53 +0900

    [SL-6] white list에 interviews, parts 추가

--- src/main/java/com/startlion/startlionserver/config/SecurityConfig.java
+++ src/main/java/com/startlion/startlionserver/config/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
             "/login/oauth2/code/google",
             "/login",
             "/application",
+            "/interviews/**",
+            "/parts/**",
 
             "/swagger-resources/**",
             "/favicon.ico",
