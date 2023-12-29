6496e1fb1e4fb568ef37c763d0e10861d400e96b
jinyoungpark00 <doghong00@naver.com>
2023-12-29 22:35:34 +0900

    [SL-16 CHORE] bold answer 추가

--- src/main/java/com/startlion/startlionserver/dto/response/interviewanswer/InterviewAnswerResponse.java
+++ src/main/java/com/startlion/startlionserver/dto/response/interviewanswer/InterviewAnswerResponse.java
@@ -5,13 +5,16 @@ import com.startlion.startlionserver.domain.entity.InterviewContent;
 public record InterviewAnswerResponse(
         Long interviewAnswerId,
         String question,
+        String boldAnswer,
         String answer
+
 ) {
 
     public static InterviewAnswerResponse of(InterviewContent interviewContent) {
         return new InterviewAnswerResponse(
                 interviewContent.getInterviewAnswerId(),
                 interviewContent.getQuestion(),
+                interviewContent.getBoldAnswer(),
                 interviewContent.getAnswer());
     }
 }
