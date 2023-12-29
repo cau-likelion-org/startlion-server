e3a32bb529574be7dece9e2d3b30979b3a30fb51
jinyoungpark00 <doghong00@naver.com>
2023-12-30 01:11:42 +0900

    [SL-16 CHORE] interview 필터링 쿼리 파라미터 추가

--- src/main/java/com/startlion/startlionserver/repository/InterviewJpaRepository.java
+++ src/main/java/com/startlion/startlionserver/repository/InterviewJpaRepository.java
@@ -3,5 +3,8 @@ package com.startlion.startlionserver.repository;
 import com.startlion.startlionserver.domain.entity.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterviewJpaRepository extends JpaRepository<Interview, Long> {
+    List<Interview> findByPart(String part);
 }
