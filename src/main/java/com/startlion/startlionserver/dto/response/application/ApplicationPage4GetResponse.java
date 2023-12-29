36a2b7372fc877504a353f25694a32949466eb66
jinyoungpark00 <doghong00@naver.com>
2023-12-29 21:09:05 +0900

    [SL-15 CHORE] interview 컬럼 2차원 Integer 배열로 변경

--- src/main/java/com/startlion/startlionserver/dto/response/application/ApplicationPage4GetResponse.java
+++ src/main/java/com/startlion/startlionserver/dto/response/application/ApplicationPage4GetResponse.java
@@ -4,11 +4,13 @@ package com.startlion.startlionserver.dto.response.application;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class ApplicationPage4GetResponse {
-    private String interview;
-    public static ApplicationPage4GetResponse of(String interview) {
+    private List<List<Integer>> interview;
+    public static ApplicationPage4GetResponse of(List<List<Integer>> interview) {
         return new ApplicationPage4GetResponse(interview);
     }
 }
