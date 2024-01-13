685bbd192ebf8bea0dbdc1d986526524c5a64ad7
unanchoi <unanc2@gmail.com>
2024-01-13 21:33:31 +0900

    [FIX] API 수정

--- src/main/java/com/startlion/startlionserver/dto/response/application/ApplyApplicationGetResponse.java
+++ src/main/java/com/startlion/startlionserver/dto/response/application/ApplyApplicationGetResponse.java
@@ -6,10 +6,20 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "지원서 조회 응답")
 public record ApplyApplicationGetResponse(
+
+        @Schema(description = "지원서 ID", example = "1")
         Long applicationId,
+
+        @Schema(description = "기수", example = "12")
         int generation,
+
+        @Schema(description = "이름", example = "홍길동")
         String name,
+
+        @Schema(description = "파트", example = "백엔드/프론트엔드/디자인/기획")
         String part,
+
+        @Schema(description = "지원서 상태", example = "지원완료, 지원중")
         String status
 ) {
 
@@ -19,7 +29,7 @@ public record ApplyApplicationGetResponse(
                 application.getGeneration(),
                 application.getName(),
                 application.getPart().getName(),
-                application.getStatus().toString()
+                application.getStatus().getName()
         );
     }
 }
