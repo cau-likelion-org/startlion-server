b08aac3c2d90c04ce868705a929b0d30050f33b1
unanchoi <unanc2@gmail.com>
2024-01-13 21:48:39 +0900

    [FIX] API Docs 수정

--- src/main/java/com/startlion/startlionserver/controller/swagger/InterviewApi.java
+++ src/main/java/com/startlion/startlionserver/controller/swagger/InterviewApi.java
@@ -4,6 +4,9 @@ package com.startlion.startlionserver.controller.swagger;
 import com.startlion.startlionserver.dto.response.interview.InterviewDetailResponse;
 import com.startlion.startlionserver.dto.response.interview.InterviewResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +18,23 @@ import java.util.List;
 public interface InterviewApi {
 
     @Operation(summary = "interview 정보 조회")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "interview 정보 조회 성공"),
+                    @ApiResponse(responseCode = "404", description = "interview 정보 없음", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+            }
+    )
     ResponseEntity<List<InterviewResponse>> getInterviews(@RequestParam(required = false) String part);
 
     @Operation(summary = "interviewId로 interview 정보 조회")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "interview 정보 조회 성공"),
+                    @ApiResponse(responseCode = "404", description = "interview 정보 없음", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+            }
+    )
     ResponseEntity<InterviewDetailResponse> getInterviewById(@PathVariable Long interviewId);
 
 }
--- src/main/java/com/startlion/startlionserver/controller/swagger/InterviewFileUploadApi.java
+++ src/main/java/com/startlion/startlionserver/controller/swagger/InterviewFileUploadApi.java
@@ -1,6 +1,9 @@
 package com.startlion.startlionserver.controller.swagger;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,5 +15,12 @@ import org.springframework.web.multipart.MultipartFile;
 public interface InterviewFileUploadApi {
 
     @Operation(summary = "파일 업로드")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "파일 업로드 성공"),
+                    @ApiResponse(responseCode = "404", description = "파일 업로드 실패", content = @Content),
+                    @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+            }
+    )
     ResponseEntity<String> uploadFile(@RequestPart MultipartFile file, @PathVariable Long interviewId);
 }
